cd17efa07ac5a21b52f2b07ad87a1b93a6f52642
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-03-28 15:57:28 -0300

    feat -select/deselect seats

--- src/components/Session.js
+++ src/components/Session.js
@@ -6,7 +6,9 @@ import axios from "axios";
 export default function Session(){
     const [movieInfo, setMovieInfo] = useState({});
     const [seatsList, setSeatsList] = useState([]);
-    const {  idSessao } = useParams();
+    const [name, setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const { idSessao } = useParams();
 
     useEffect(() => {
         const request = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`);
@@ -18,29 +20,71 @@ export default function Session(){
         request.catch(() => console.log("erro"));
     }, []);
 
+    function selectSeat(id){
+        seatsList.forEach(seat => {
+            if (seat.id === id){
+                seat.isSelected = true;
+            }
+        })
+        setSeatsList([...seatsList]);
+    }
+    function deselectSeat(id){
+        seatsList.forEach(seat => {
+            if (seat.id === id){
+                seat.isSelected = false;
+            }
+        })
+        setSeatsList([...seatsList]);
+    }
     function Seats(){
         return(
             <div className="seats">
-                {seatsList.map(e => e.isAvailable ? (<AvailableSeat key={e.id} id={e.id} name={e.name}/>) : (<TakenSeat key={e.id} id={e.id} name={e.name} />))}
+                {seatsList.map(e => e.isAvailable ? (e.isSelected ? (<AvailableSeatSelected key={e.id} id={e.id} name={e.name} />) : (<AvailableSeat key={e.id} id={e.id} name={e.name} />)) : (<TakenSeat key={e.id} id={e.id} name={e.name} />))}
             </div>
         );
     }
     function AvailableSeat(props){
         const {id, name} = props;
         return(
-            <div className="seat available" key={id}>
+            <div className={`available seat`} key={id} onClick={()=>selectSeat(id)}>
                  <p>{name.padStart(2, '0')}</p>
             </div>
         );
     }
+
+    function AvailableSeatSelected(props){
+        const {id, name} = props;
+        return(
+            <div className={`available seat selected`} key={id} onClick={()=>deselectSeat(id)}>
+                 <p>{name.padStart(2, '0')}</p>
+            </div>
+        );
+    }
+
     function TakenSeat(props){
         const {id, name} = props;
         return(
-            <div className="seat taken" key={id}>
+            <div className="seat taken" key={id} onClick={() => alert("Esse assento não está disponível")}>
                 <p>{name.padStart(2, '0')}</p>
             </div>
         );
     }
+
+    function sendRequest(){
+        if (name === "" || cpf === ""){
+            alert("Preencha os campos necessários antes de continuar!")
+        }else{
+            let idsList = [];// seatsList.filter(function (el) {return el.isSelected});
+            seatsList.forEach(e => {
+                if (e.isSelected){
+                    idsList.push(e.id);
+                }
+            });
+            console.log(idsList);
+        }
+    }
+
+    
 
     if (seatsList.length === 0){
         return(
@@ -52,6 +96,13 @@ export default function Session(){
        <div className="session">
            <p className="top-text">Selecione o(s) assento(s)</p>
            <Seats />
+           <div className="container">
+            <div className="text-session">Nome do comprador:</div>
+            <input placeholder="Digite seu nome..." onChange={event => setName(event.target.value)} />
+            <div className="text-session">Cpf do comprador:</div>  
+            <input placeholder="Digite seu cpf..." onChange={event => setCpf(event.target.value)} />
+            <button className="res-button" onClick={sendRequest}>Reservar assento(s)</button>
+           </div>
            <div className="footer">
                 <img src={movieInfo.movie.posterURL} alt={movieInfo.movie.title}/>
                 <div className="movie-info">
